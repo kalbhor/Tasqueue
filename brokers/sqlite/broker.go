09c49e9446450b9e663e3aed864f68e55859e60a
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-09-30 23:59:59 +0530

    refactor: add constructors to create broker and results

--- brokers/sqlite/broker.go
+++ brokers/sqlite/broker.go
@@ -2,11 +2,44 @@ package sqlite
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"log/slog"
 	"time"
 )
 
-type Broker struct{}
+type Broker struct {
+	db  *sql.DB
+	log *slog.Logger
+}
+
+type Options struct {
+	DataSource string
+}
+
+func New(options Options, lo *slog.Logger) (*Broker, error) {
+	db, err := sql.Open("sqlite3", options.DataSource)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS jobs(
+            id VARCHAR PRIMARY KEY,
+            queue VARCHAR NOT NULL,
+            msg TEXT NOT NULL,
+            timestamp DATE DEFAULT(datetime('now', 'localtime'))
+        );
+    `)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Broker{
+		db:  db,
+		log: lo,
+	}, nil
+}
 
 func (broker *Broker) Enqueue(ctx context.Context, msg []byte, queue string) error {
 	return fmt.Errorf("not implemeted")
--- results/sqlite/results.go
+++ results/sqlite/results.go
@@ -2,10 +2,38 @@ package sqlite
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"log/slog"
 )
 
-type Results struct{}
+type Results struct {
+	db  *sql.DB
+	log *slog.Logger
+}
+
+type Options struct {
+	DataSource string
+}
+
+func New(options Options, lo *slog.Logger) (*Results, error) {
+	db, err := sql.Open("sqlite3", options.DataSource)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS results(
+            id VARCHAR PRIMARY KEY,
+            msg TEXT NOT NULL
+        );
+    `)
+
+	return &Results{
+		db:  db,
+		log: lo,
+	}, nil
+}
 
 func (results *Results) Get(ctx context.Context, id string) ([]byte, error) {
 	return nil, fmt.Errorf("not implemented")
